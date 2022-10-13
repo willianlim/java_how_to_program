f7619b4ecf5137d93438b48d8ccc024f61950421
Willian-code <willianrosendodelima@outlook.com>
2022-10-13 12:39:23 -0300

    performing comparisons using the 'if'

---
+++ src/ch02/ex02_16/Comparison.java
@@ -0,0 +1,36 @@
+/*
+ * 2.16 (comparing Integers) Write an application that asks the users to enter
+ * two integers, obtains them from the user and displays the larger number
+ * followed by the words "is larger". If the numbers are equal, print the
+ * message "These numbers are equal". Use the techniques shown in Fig. 2.15
+ */
+package ch02.ex02_16;
+
+import java.util.Scanner;
+
+public class Comparison {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = scanner.nextInt();
+        scanner.close();
+
+        if (number1 == number2) {
+            System.out.println("These numbers are equal");
+        }
+
+        if (number1 > number2) {
+            System.out.printf("%d is larger", number1);
+        }
+
+        if (number1 < number2) {
+            System.out.printf("%d is larger", number2);
+        }
+    }
+
+}
