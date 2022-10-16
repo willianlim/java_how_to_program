a6617ecdd4f95967ef4a20dcdfc804b53dff9b1b
Willian-code <willianrosendodelima@outlook.com>
2022-10-15 21:48:49 -0300

    the number of asterisks is equivalent to the user input value

---
+++ src/ch05/ex05_16/Asterisks.java
@@ -0,0 +1,14 @@
+package ch05.ex05_16;
+
+import java.util.Scanner;
+
+public class Asterisks {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+        String[] bars = new String[5];
+
+
+    }
+}
