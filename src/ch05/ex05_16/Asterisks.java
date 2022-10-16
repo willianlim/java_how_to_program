b2638d28230f30746629cd0c19d4790156a19bfc
Willian-code <willianrosendodelima@outlook.com>
2022-10-16 17:52:41 -0300

    printing asterisks

--- src/ch05/ex05_16/Asterisks.java
+++ src/ch05/ex05_16/Asterisks.java
@@ -9,6 +9,20 @@ public class Asterisks {
         int[] numbers = new int[5];
         String[] bars = new String[5];
 
+        for (int i = 0; i < numbers.length;)  {
+            System.out.print("Enter a number between 1 and 30: ");
+            numbers[i] = input.nextInt();
 
+            if (numbers[i] < 1 || numbers[i] > 30)
+                System.out.println(" - Error: number must is between 1-30. Try again!");
+            else {
+                for (int j = 0; j < numbers[i]; j++)
+                    bars[i] += "*";
+                i++;
+            }
+        }
+        input.close();
+        for (int i = 0; i < numbers.length; i++)
+            System.out.printf("%02d = %s%n", numbers[i], bars[i]);
     }
 }
