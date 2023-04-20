ee27eedd4a65d85fd31073a4c0e2115e0acb04d4
willianlim <willianrosendodelima@outlook.com>
2023-04-20 12:32:08 -0300

    exibindo texto com printf

---
+++ src/ch02/Welcome3.java
@@ -0,0 +1,9 @@
+package ch02;
+
+public class Welcome3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome\nto\nJava\nPrograming!");
+    }
+}
---
+++ src/ch02/Welcome4.java
@@ -0,0 +1,10 @@
+package ch02;
+
+public class Welcome4 {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%s%n%s%n",
+                "Welcome to", "Java programming!");
+    }
+}
